28435392244dffd9cf72a813df35bcb51cd166ba
Mauricio Diaz <mauricio809diaz@gmail.com>
2024-10-04 04:52:56 -0400

    Added simple unit test and the maven surefire plugin to run unit tests with maven

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,56 @@
+/*
+	Mauricio Diaz
+	CEN3024C
+	July 18 , 2024
+	Software Development 1
+
+	BookTest Class: This class is for holding the Unit Tests for the Book class.
+*/
+
+import com.mauricio.librarymanagementsystem.Book;
+import com.mauricio.librarymanagementsystem.Database;
+import com.mauricio.librarymanagementsystem.Functions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest { // Visible within same package, not accessible by other packages. This class has the standard access modifier of "package-private"
+
+    Book testBook = new Book();
+
+    // Constructor for unit test.
+    public BookTest(){
+        setTestBook();
+    }
+
+    public void setTestBook(){
+        testBook.setId(999);
+        testBook.setTitle("Unit Testing with Mauricio Diaz");
+        testBook.setAuthor("Unit Test");
+        testBook.setReleaseYear(1901);
+        testBook.setBarcode("UnitTest");
+    }
+
+    // Small JUnit test.
+    @Test
+    void addRemoveBookTest() {
+
+        File myObj = new File("LMS_DATABASE");
+
+        if (myObj.exists()){
+            assertEquals(1, Functions.addBook(1, testBook));
+            testBook.setTitle("Unit Testing with Mauricio Diaz");
+            assertEquals("Deleting book with title: Unit Testing with Mauricio Diaz", Functions.deleteBookTitle(testBook));
+        }else{
+            Database.createDatabase();
+            assertEquals(1, Functions.addBook(1, testBook));
+            testBook.setTitle("Unit Testing with Mauricio Diaz");
+            assertEquals("Deleting book with title: Unit Testing with Mauricio Diaz", Functions.deleteBookTitle(testBook));
+            myObj.delete();
+        }
+
+    }
+
+}
